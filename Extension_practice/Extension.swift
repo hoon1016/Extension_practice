7ff08f5257274cbfb71531a4b98b4d5f7f6626f8
hoon1016 <wjsrms76@gmail.com>
2021-08-04 17:16:40 +0900

    swift study

---
+++ Extension_practice/Extension.swift
@@ -0,0 +1,18 @@
+//
+//  Extension.swift
+//  Extension_practice
+//
+//  Created by 임재훈 on 2021/08/03.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    
+    
+    func sayHi() {
+        print("하이!!")
+    }
+    
+}
